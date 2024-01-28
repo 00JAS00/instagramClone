28b19e7ea200370f3e32b72be90374ec0e0d9044
Luis Reyes <yuiftw05@gmail.com>
2024-01-28 11:14:13 -0600

    fix: usando las rutas en el app.js, la ruta principal en la que se basaran las demas seran: instaclone

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-// const routes = require('./routes/index')
+const routes = require('./routes/index')
 
 const server = express()
 
@@ -17,6 +17,8 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+
+server.use('/instaclone', routes);
 
 
 // Error catching endware.
