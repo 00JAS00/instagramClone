cecf6fb8dee11c4f0e374b649e0e3ffbda096593
Luis Reyes <yuiftw05@gmail.com>
2024-01-28 11:27:15 -0600

    fix: usando el create en lugar del findOrCreate

--- Server/src/controllers/PostUser.js
+++ Server/src/controllers/PostUser.js
@@ -3,14 +3,14 @@ const {User} = require('../db')
 const postUser = async(req,res)=>{
     try {
         const {username,email, password} = req.body
-        if(!email || password || username) return res.status(400).send("Empty inputs")
+        if(!email || !password || !username) return res.status(400).send("Empty inputs")
 
-        const newUser = await User.findOrCreate({
-            where: {
+        const newUser = await User.create({
+
                 email, 
                 password,
                 username
-            }
+            
         })
         res.status(201).json(newUser)
     } catch (error) {
