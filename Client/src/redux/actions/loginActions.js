ba9f6c58d7005a54606a3b1e18e42d027274c2d1
Luis Reyes <yuiftw05@gmail.com>
2024-01-28 22:58:30 -0600

    feat: iniciando redux con sus carpetas y estructura

---
+++ Client/src/redux/actions/loginActions.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+import {
+LOGIN,
+LOGOUT
+} from '../action_types/loginActionTypes'
+
+const URL = "http://localhost:3001/instaclone/"
+
+export const getLogin = (user,password) => async (dispatch)=>{
+    try {
+        const response = await axios(`${URL}`)
+        const {data} = response
+    } catch (error) {
+        window.alert(error)
+    }
+}
---
+++ Client/src/redux/reducers/loginReducer.js
@@ -0,0 +1,13 @@
+
+const initailState = {
+    isAuth: null
+}
+
+const loginReducer = (state = initailState, action)=>{
+    switch(action.type){
+        default:
+            return state
+    }
+}
+
+export default loginReducer
