917f0b2fdc773de6e2313de9844c2e2b64ea5cf0
Luis Reyes <yuiftw05@gmail.com>
2024-01-30 19:45:51 -0600

    feat: styles: using 90% of width with Feed container

--- Client/src/components/Feed/Feed.jsx
+++ Client/src/components/Feed/Feed.jsx
@@ -4,7 +4,7 @@ import styles from './Feed.module.css'
 const Feed = ()=>{
     return(
         <div className={styles.Feed}>
-            Post container
+            las publicaciones iran aca
         </div>
     )
 }
--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,13 +1,16 @@
 //HOOKS
 import { useState, useEffect } from "react";
 
+//styles
+import styles from './Home.module.css'
+
 //COMPONENTS
 import Sidebar from "../Sidebar/Sidebar";
 import Feed from "../Feed/Feed";
 
 const Home = ()=>{
     return(
-        <main>
+        <main className={styles.main}>
             <Feed/>
             <Sidebar/>
         </main>
